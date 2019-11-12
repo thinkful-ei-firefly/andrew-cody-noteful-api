32c26a8bbd949e4638b230b7de0c5d903bb17002
krill <lpmaze@gmail.com>
2019-11-12 12:13:16 -0500

    yest

--- src/folders/folderRouter.js
+++ src/folders/folderRouter.js
@@ -14,7 +14,7 @@ foldersRouter
     // .catch(next) if we have error handling middleware or we don't need next b/c
   })
   .post(bodyParser, (req, res, next) => {
-    console.log(req.body)
+    // console.log(req.body)
     const { name } = req.body
 
     if (!name || name.length < 1) {
@@ -36,28 +36,31 @@ foldersRouter
       res.json(fdata)
     )//.catch(next) don't need catch/next b/c app.js ends with the error handler?
   })
-  .delete((req, res, next) => {
-    const id = req.params.id
+  .delete(bodyParser, (req, res, next) => {
+    // const id = req.params.id
+    // console.log('req body:',req.body)
+    const id = req.body.folderId
     FoldersService.deleteFolder(req.app.get('db'), id)
       .then(dbRes => {
         res.json(dbRes)
       })
   })//.catch(next) don't need catch/next b/c app.js ends with the error handler?
   .patch(bodyParser, (req, res, next) => {
-    const id = req.params.id
-    const { folder_name } = req.body
+    // const id = req.params.id
+    const { folder_name, id } = req.body
 
+    // console.log(req.body)
     if (!folder_name) {
       return res.status(400).send('Must provide folder name')
     }
 
     const newFolder = { folder_name }
 
-    FoldersService.updateFolder(req.app.get('db'), newFolder, id).then(
-      dbRes => {
-        res.json(dbRes)
-      }
-    )//.catch(next) don't need catch/next b/c app.js ends with the error handler?
+    // console.log(newFolder)
+    FoldersService.updateFolder(req.app.get('db'), newFolder, id).then(dbRes => {
+      // console.log(dbRes[0])
+      return res.status(200).json(dbRes)
+    })//.catch(next) don't need catch/next b/c app.js ends with the error handler?
   })
 
 module.exports = foldersRouter
