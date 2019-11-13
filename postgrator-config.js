51f6166394b9b9d7d60c1998ba1a310d23cec316
krill <lpmaze@gmail.com>
2019-11-12 22:44:37 -0500

    postgratpr

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,8 @@
 module.exports = {
   driver: 'pg',
   connectionString:
-    process.env.NODE_ENV === 'test'
-      ? 'postgres://dunder-mifflin@localhost/noteful_test'
-      : 'postgres://dunder-mifflin@localhost/noteful'
+    process.env.NODE_ENV == 'development' || 'test'
+      ? process.env.DB_URL
+      : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL
 }
