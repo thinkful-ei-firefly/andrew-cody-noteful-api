146dbb02ae8dec1a5f90f3ad16e91bd0ad025b1d
krill <lpmaze@gmail.com>
2019-11-13 15:42:22 -0500

    still working on migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,10 +7,10 @@ module.exports = {
     process.env.NODE_ENV == 'development' || 'test'
       ? process.env.DB_URL
       : process.env.DATABASE_URL,
-  ssl: !!process.env.SSL,
-  host: process.env.MIGRATION_DB_HOST,
-  port: process.env.MIGRATION_DB_PORT,
-  database: process.env.MIGRATION_DB_NAME,
-  username: process.env.MIGRATION_DB_USER,
-  password: process.env.MIGRATION_DB_PASS
+  // ssl: !!process.env.SSL,
+  // host: process.env.MIGRATION_DB_HOST,
+  // port: process.env.MIGRATION_DB_PORT,
+  // database: process.env.MIGRATION_DB_NAME,
+  // username: process.env.MIGRATION_DB_USER,
+  // password: process.env.MIGRATION_DB_PASS
 }
